b94ab8644795785089ca99a50d216e9d995b9866
rafaelclima <rafaelclima.ti@gmail.com>
2025-07-28 21:24:47 -0300

    feat: criando estrutura com spring boot

---
+++ src/main/java/br/com/rafaellima/forumhub/ForumHubApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaellima.forumhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaellima/forumhub/ForumHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaellima.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

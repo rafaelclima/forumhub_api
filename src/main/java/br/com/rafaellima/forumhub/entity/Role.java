6690f76c3848add7618b3e403289e4395ea9dc82
rafaelclima <rafaelclima.ti@gmail.com>
2025-07-30 22:49:33 -0300

    Feat: changed method to add role an user

--- src/main/java/br/com/rafaellima/forumhub/entity/Role.java
+++ src/main/java/br/com/rafaellima/forumhub/entity/Role.java
@@ -1,57 +1,17 @@
 package br.com.rafaellima.forumhub.entity;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.proxy.HibernateProxy;
+public enum Role {
+   ADMIN,
+   USER;
 
-import java.util.Objects;
-
-@Entity(name = "Role")
-@Table(name = "roles")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Role {
-
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
-
-   @Column(name = "nome", nullable = false, unique = true)
-   private String nome;
-
-   @Override
-   public final boolean equals(Object o) {
-
-      if (this == o) return true;
-      if (o == null) return false;
-      Class<?> oEffectiveClass = o instanceof HibernateProxy ?
-            ((HibernateProxy) o)
-                  .getHibernateLazyInitializer()
-                  .getPersistentClass() :
-            o.getClass();
-      Class<?> thisEffectiveClass = this instanceof HibernateProxy ?
-            ((HibernateProxy) this)
-                  .getHibernateLazyInitializer()
-                  .getPersistentClass() :
-            this.getClass();
-      if (thisEffectiveClass != oEffectiveClass) return false;
-      Role role = (Role) o;
-      return getId() != null && Objects.equals(getId(), role.getId());
+   public String getNome() {
+      return this.name();
    }
-
-   @Override
-   public final int hashCode() {
-
-      return this instanceof HibernateProxy ?
-            ((HibernateProxy) this)
-                  .getHibernateLazyInitializer()
-                  .getPersistentClass()
-                  .hashCode() :
-            getClass().hashCode();
+   public static Role fromString(String roleName) {
+      try {
+         return Role.valueOf(roleName.toUpperCase());
+      } catch (IllegalArgumentException e) {
+         throw new IllegalArgumentException("Role '" + roleName + "' is not valid.");
+      }
    }
 }
